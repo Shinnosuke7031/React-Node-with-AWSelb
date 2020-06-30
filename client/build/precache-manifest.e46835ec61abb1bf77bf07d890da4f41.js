57d951a953d2dd137abc943a891c816dbb969a6a
iino <sengoku731sin@gmail.com>
2020-06-30 17:01:41 +0900

    improving deploy

---
+++ client/build/precache-manifest.e46835ec61abb1bf77bf07d890da4f41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b0349be44adf78f4365700d9187b8d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "477c981f398e95c9137e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "65f542964af01fc5aa1b",
+    "url": "/static/js/2.2dcf5f33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2dcf5f33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "477c981f398e95c9137e",
+    "url": "/static/js/main.cddd6614.chunk.js"
+  },
+  {
+    "revision": "4d9af2a9620265095b18",
+    "url": "/static/js/runtime-main.e8bda96d.js"
+  }
+]);
