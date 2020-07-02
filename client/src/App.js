000cf692c677eb38bad04af16dcae71ade4dde16
iino <sengoku731sin@gmail.com>
2020-07-02 20:26:10 +0900

    Create -add new comment at First-

--- client/src/App.js
+++ client/src/App.js
@@ -5,28 +5,74 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      flower: []
+      isSubmit: false,
+      data: [],
+      newName: "",
+      newText: ""
     }
     this.getData();
   }
+
   getData() {
     fetch('/data')
       .then(response => response.json())
-      .then(data => {
+      .then(temp => {
         this.setState({
-          flower: data.commentInfo
+          data: temp.commentInfo
         });
       });
   }
 
+  handleSubmit() {
+    if(this.state.isSubmit) {
+      this.setState({isSubmit: false});
+    } else {
+      this.setState({isSubmit: true});
+    }
+    
+    //console.log(this.state.isSubmit);
+    return false;
+  }
+
+  
+
   render() {
+
+    let newMessage;
+    if (this.state.isSubmit) {
+      newMessage = (
+        <div className="newMes">
+          <p>送信完了!!</p>
+          <form onSubmit={()=>this.handleSubmit()}>
+            <input type="submit" value="OK" />
+          </form>
+        </div>
+      );
+    } else {
+      newMessage = (
+        <div className="newMes">
+          <form onSubmit={()=>this.handleSubmit()}>
+            <p style={{fontSize: "20px", textDecoration: "underline"}}>新規コメント</p>
+            <p>名前</p>
+            <input value="匿名" name={this.state.newName} />
+            <p>コメント内容</p>
+            <div className="newT">
+              <textarea name={this.state.newText} />
+              <input type="submit" value="送信" />
+            </div>
+            
+          </form>
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>自由に話そう</h1>
         <hr style={{backgroundColor: "black"}}></hr>
         <div className="showCom">
         
-        {this.state.flower.map((temp) => 
+        {this.state.data.map((temp) => 
           <div>
             <div className="showName">
               <p>{temp.name}</p>
@@ -39,6 +85,10 @@ class App extends Component {
         )}
         <hr className="bottomLine"></hr>
         </div>
+          
+      <div>
+        {newMessage}
+      </div>
         
       </div>
     );
--- client/src/Header.js
+++ client/src/Header.js
@@ -5,8 +5,7 @@ const Header = () =>  {
 
   return (
     <div className="header">
-      <h1>リアルタイムチャット</h1>
-      
+      <h1>チャットAPP</h1>
     </div>
   );
 }
